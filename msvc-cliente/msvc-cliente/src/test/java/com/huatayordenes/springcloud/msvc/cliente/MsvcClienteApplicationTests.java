eb4803c95668d6e6c50c8b19b3bf05d372c7e7ab
Dozy <ahuatayc18_2@unc.edu.pe>
2024-02-18 08:23:14 -0500

    Agregamos microservicio cliente

---
+++ msvc-cliente/msvc-cliente/src/test/java/com/huatayordenes/springcloud/msvc/cliente/MsvcClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huatayordenes.springcloud.msvc.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
