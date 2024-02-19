553009a4d5649c7909e6afbe705d5e4bd6174c84
Dozy <ahuatayc18_2@unc.edu.pe>
2024-02-19 08:54:00 -0500

    Creacion clase cliente, atributos y su persitencia

---
+++ msvc-cliente/msvc-cliente/src/main/java/com/huatayordenes/springcloud/msvc/cliente/models/entity/Cliente.java
@@ -0,0 +1,57 @@
+package com.huatayordenes.springcloud.msvc.cliente.models.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    @Column(unique = true)
+    private String email;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
