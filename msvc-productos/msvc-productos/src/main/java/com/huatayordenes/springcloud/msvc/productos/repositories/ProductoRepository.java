120645ba593093199fa273590b7e9bb9129f2ab9
Dozy <ahuatayc18_2@unc.edu.pe>
2024-03-01 12:18:16 -0500

    Implementacion metodo buscar por nombre

--- msvc-productos/msvc-productos/src/main/java/com/huatayordenes/springcloud/msvc/productos/repositories/ProductoRepository.java
+++ msvc-productos/msvc-productos/src/main/java/com/huatayordenes/springcloud/msvc/productos/repositories/ProductoRepository.java
@@ -3,5 +3,8 @@ package com.huatayordenes.springcloud.msvc.productos.repositories;
 import com.huatayordenes.springcloud.msvc.productos.models.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ProductoRepository extends CrudRepository<Producto,Long> {
+        Optional<Producto> findByName(String name);
 }
